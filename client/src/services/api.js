b4f43c3326edd4620befd655ceb2ea661030ee2c
AbhijeetSRawat <rawatab2@gmail.com>
2025-10-31 23:11:01 +0530

    link changed

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
-// const BASE_URL = "http://localhost:4000/api/v1"
+const BASE_URL = "http://localhost:4000/api/v1"
 
-const BASE_URL = "https://startup-healer-backend.onrender.com/api/v1"
+// const BASE_URL = "https://startup-healer-backend.onrender.com/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
